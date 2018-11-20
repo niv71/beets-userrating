4b40704935fe5bca04366ce4e29729192402d6b1
Michael Wawrzyniak <mwawrzyniak@cmstactical.net>
2018-11-20 16:06:26 -0500

    Updated setup.py with my info

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup
 
 setup (
     name                 = 'beets-userrating',
-    author               = 'Michael Alan Dorman',
-    author_email         = 'mdorman@jaunder.io',
+    author               = 'Michael Wawrzyniak',
+    author_email         = 'mwawrzyniak@cmstactical.net',
     description          = "A plugin for managing user's per-track ratings in the music geek's media organizer",
     install_requires     = ['beets >= 1.4.3'],
-    license              = 'GLPv3',
+    license              = 'GPLv3',
     namespace_packages   = ['beetsplug'],
     packages             = ['beetsplug'],
     platforms            = 'ALL',
-    url                  = 'https://github.com/mdorman/beets-userrating',
-    version              = '0.0.1',
+    url                  = 'https://github.com/niv71/beets-userrating',
+    version              = '0.0.2',
 )
